adc79b710df750921241e083164bb66c069fc302
RKSunan <rajkumarisunanda@gmail.com>
2020-11-02 22:32:15 +0530

    Third commit

--- empWageCalcutaltion.sh
+++ empWageCalcutaltion.sh
@@ -1,1 +1,13 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+echo "Day   Employee Attandence"
+for i in $(seq 1 20)
+do
+day[$i]=$((RANDOM %2))
+if [ ${day[$i]} == 1 ]
+then
+        echo " $i     Present"
+else
+        echo " $i     Absent"
+fi
+done
+

26c13382813b3cb05be50f19d8e68ee9026b4689
RKSunan <rajkumarisunanda@gmail.com>
2020-11-02 23:00:17 +0530

    Fourth commit

--- empWageCalculation.sh
+++ empWageCalculation.sh
@@ -1,14 +1,19 @@
 #! /bin/bash
-echo "Day   Employee Attandence"
 for i in $(seq 1 20)
 do
 day[$i]=$((RANDOM %2))
 if [ ${day[$i]} == 1 ]
 then
-        echo " $i     Present"
+                wage[$i]=`expr 8  \* 20  `
+ #               echo "Daily wage : ${wage[i]}"
 else
-        echo " $i     Absent"
+        wage[$i]=0
 fi
 done
-
+echo "day  daily_wage"
+num=0
+for i in ${!wage[@]}
+do
+        echo "$i      ${wage[$i]}"
+done
 

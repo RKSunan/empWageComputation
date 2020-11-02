7eb035e493d04bca40ca55fab83be195eef392dd
RKSunan <rajkumarisunanda@gmail.com>
2020-11-03 00:26:23 +0530

    CalculateWorkHour

--- empWageCalculation.sh
+++ empWageCalculation.sh
@@ -1,43 +1,50 @@
 #! /bin/bash
-        hr=8
-        hr2=4
-        #hour[0]=0
-        totalhour=0
-       for i in $(seq 1 100)
+findHours(){
+if [ $1 -le 20 ]
+then
+totalhour=0
+for i in $(seq 1 $1)
         do
-        if [[  $totalhour -lt 200  && $i -le 20 ]]
-        then
-        day[$i]=$((RANDOM %2))
+      day[$i]=$((RANDOM %2))
         if [ ${day[$i]} == 1 ]
         then
         full[$i]=$((RANDOM%2))
         if [ ${full[i]} == 1 ]
                 then
+                hr[i]=8
                 wage[i]=`expr 8  \* 20  `
-                 hour[i]=$(( totalhour + hr ))
-                 totalhour=${hour[$i]}
         elif [ ${full[i]} == 0 ]
         then
                 wage[i]=`expr 4 \* 20 `
-                  hour[i]=$(( totalhour + hr2 ))
-                 totalhour=${hour[$i]}
+                        hr[i]=4
         fi
         else
         full[$i]=0
         wage[$i]=0
-        hour[$i]=$totalhour
+        hr[$i]=0
         fi
-        fi
-        done
-        echo "day  daily_wage  Total_wage    Total_Hour "
-        num=0
-        for i in ${!wage[@]}
-        do
-        sum=$(( $num + ${wage[$i]} ))
-        num=$sum
-        echo "$i      ${wage[$i]}              $num          ${hour[$i]}"
-        done
-        echo "The wage of the month is : $num"
-        echo "The  total moth work in a month : $totalhour"
+done
+echo "day    hour  Total_hour"
+for i in ${!day[@]}
+do
+sum=$(( ${hr[$i]} + $totalhour ))
+totalhour=$sum
+echo "$i     ${hr[$i]}          $totalhour"
+if [  $i -eq $1 ]
+then
+echo "The employee work ${hr[$i]} hr on $i day and total $totalhour hour"
+fi
+done
+else
+        echo "Invalid day"
+fi
+}
+while :
+do
+echo "Enter a day"
+read n
+findHours $n
+exit 1
+done
 
 
